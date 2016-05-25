0e9a887f1e3620dbebc176e0fb2717a6275a88e3
Nana Adane <nanaofosuheneadane@gmail.com>
2016-05-25 00:59:43 +0000

    Write config file

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: './dist/batbelt.js'
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'jshint-loader'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+   extensions: ['','.js']
+ },
+  watch: true
+};
