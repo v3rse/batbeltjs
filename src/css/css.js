a99106bfeaacc8ad043de4cbd0c23b67aade21ba
Nana Adane <nanaofosuheneadane@gmail.com>
2016-05-26 02:09:22 +0000

    Add css manipulation function

--- src/css/css.js
+++ src/css/css.js
@@ -1,3 +1,24 @@
-module.exports = function (){
-  return "Hello Tests!";
+module.exports = function (element,key,value){
+  if (arguments.length > 1) {//check number of arguments
+    if(typeof element === 'undefined'){
+      console.error('Provide element');
+      return;
+    }
+    if (typeof key === 'string') {//if second argument is a string
+      if (typeof value !== 'undefined') {//and third is defined
+        //set single property
+        element.style[key] = value;
+      }else {//if third is undefined
+        //get CSS property value
+        return window.getComputedStyle(element,null)[key];
+      }
+    }else if( typeof key === 'object'){//if second argument is an object
+        //set multiple properties
+        for(var i in key){
+          element.style[i] = key[i];
+        }
+    }
+  }else {//if fewer than one element is set then through error.
+      console.error('Provide the necessary arguments');
+  }
 };
